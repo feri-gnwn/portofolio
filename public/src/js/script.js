8fca75f7de311aaac1220f5995aa13f6770f33f3
unknown <ferigunawan1505@gmail.com>
2025-02-17 03:42:12 +0800

    Melengkapi web dengan portofolio dan animasi

--- public/src/js/script.js
+++ public/src/js/script.js
@@ -1,9 +1,9 @@
 //Navbar fixed
-window.onscroll = function(){
+window.onscroll = function () {
     const header = document.querySelector('header');
     const fixedNav = header.offsetTop;
 
-    if(window.pageYOffset> fixedNav){
+    if (window.pageYOffset > fixedNav) {
         header.classList.add('navbar-fixed')
     } else {
         header.classList.remove
@@ -14,25 +14,42 @@ window.onscroll = function(){
 const hamburger = document.querySelector('#hamburger');
 const navMenu = document.querySelector('#nav-menu');
 
-hamburger .addEventListener('click', function(){
+hamburger.addEventListener('click', function () {
     hamburger.classList.toggle('hamburger-active');
     navMenu.classList.toggle('hidden');
 });
 
 //audio button
 const audio = document.getElementById('audio');
-        const playButton = document.getElementById('playButton');
-        const playIcon = document.getElementById('playIcon');
-        const pauseIcon = document.getElementById('pauseIcon');
+const playButton = document.getElementById('playButton');
+const playIcon = document.getElementById('playIcon');
+const pauseIcon = document.getElementById('pauseIcon');
 
-        playButton.addEventListener('click', () => {
-            if (audio.paused) {
-                audio.play();
-                playIcon.classList.add('hidden');
-                pauseIcon.classList.remove('hidden');
-            } else {
-                audio.pause();
-                playIcon.classList.remove('hidden');
-                pauseIcon.classList.add('hidden');
-            }
-        });
+playButton.addEventListener('click', () => {
+    if (audio.paused) {
+        audio.play();
+        playIcon.classList.add('hidden');
+        pauseIcon.classList.remove('hidden');
+    } else {
+        audio.pause();
+        playIcon.classList.remove('hidden');
+        pauseIcon.classList.add('hidden');
+    }
+});
+
+// Alert pesan
+const kirimBtn = document.getElementById('kirimBtn');
+const alertBox = document.getElementById('alert');
+const closeAlert = document.getElementById('closeAlert');
+
+// Tampilkan alert saat tombol kirim diklik
+kirimBtn.addEventListener('click', () => {
+    alertBox.classList.remove('hidden');
+    alertBox.classList.add('block');
+});
+
+// Tutup alert saat tombol close diklik
+closeAlert.addEventListener('click', () => {
+    alertBox.classList.remove('block');
+    alertBox.classList.add('hidden');
+});
